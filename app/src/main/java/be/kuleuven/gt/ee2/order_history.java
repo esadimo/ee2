cba3df0432b27345b821df3961b75f7b1778d615
Esat Caglayan <esat.caglayan@student.kuleuven.be>
2024-03-15 11:23:13 +0100

    ee2 15 march 11:23

---
+++ app/src/main/java/be/kuleuven/gt/ee2/order_history.java
@@ -0,0 +1,14 @@
+package be.kuleuven.gt.ee2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class order_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_history);
+    }
+}
