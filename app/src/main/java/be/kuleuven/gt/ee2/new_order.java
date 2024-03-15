db36da42c4b0267122f3ee2cc5b4bb5164293e9e
Esat Caglayan <esat.caglayan@student.kuleuven.be>
2024-03-15 08:29:17 +0100

    ee2 first commit

---
+++ app/src/main/java/be/kuleuven/gt/ee2/new_order.java
@@ -0,0 +1,14 @@
+package be.kuleuven.gt.ee2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order);
+    }
+}
